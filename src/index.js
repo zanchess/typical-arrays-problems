7e260a32274de8e97a1961e19fd575d79a148dec
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-19 22:56:28 +0300

    feat: end typical array problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+		let arr = array.sort((a, b) => {
+			return a - b;
+		});
+		return arr[0];
+  } else{
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+		let arr = array.sort((a, b) => {
+			return a - b;
+		});
+		return arr[arr.length-1];
+  } else{
+		return 0;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+		var total = array.reduce(function(a, b) {
+			return a + b;
+	});
+
+	return total/array.length;
+  } else{
+		return 0;
+	}
 }
